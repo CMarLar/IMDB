57f9a28860ee648c7fa6ec25b864867bc6843089
CMarLar <c.marina.larrosa@gmail.com>
2022-10-25 11:48:56 +0200

    Parse method added to readFile.ts

--- readFile.ts
+++ readFile.ts
@@ -5,4 +5,10 @@ const path = "./imdbBBDD.json"
 
 //fs.readFileSync (pathToFile, {encoding, flag})
 let data = fs.readFileSync(path,"utf8")
-console.log(data);
+//console.log(data);
+
+//New IMDB object
+let newIMDB = JSON.parse(data);
+console.log(newIMDB);
+
+
